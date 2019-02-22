8a9d47d87347b4058381d942e870a9ab8714b0c2
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-22 18:52:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,10 +1,10 @@
 def create_an_empty_array
-  my_arr= []
+  my_arr= Array.new
   return my_arr
 end
 
 def create_an_array
-  
+  my_arr = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
